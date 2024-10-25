f345144e5bbcfae73516fb0e021e923ba73f834e
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-10-25 04:48:14 +0200

    Fixed Calculation errors

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
  */
 
 // Given Parameters
-const InitVelocity = 10000; // Initial velocity (km/h)
+const InitVelocityKmph = 10000; // Initial velocity (km/h)
+const InitVelocityMps = InitVelocityKmph * (1000 / 3600)
 const Acceleration = 3; // Space Crafts aceleration (m/s^2)
 const OneHourInSeconds = 3600; // duration of the calculation in seconds (1 hour)
 const InitDistance = 0; // distance from reference point (km)
@@ -14,13 +15,13 @@ const Initfuel = 5000; // Initial fuel (kg)
 const FuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 // Pick up an error with how the function below is called and make it robust to such errors
-const CalcNewVelocity = ( Acceleration ,InitVelocity, OneHourInSeconds) => { 
-  return InitVelocity + (Acceleration *OneHourInSeconds)
+const CalcNewVelocity = ( Acceleration ,InitVelocityMps, OneHourInSeconds) => { 
+  return (InitVelocityMps + (Acceleration * OneHourInSeconds))*(3600/1000)
 }
 
-const NewDistance = InitDistance + (InitVelocity * OneHourInSeconds) //calcultes new distance
-const RemainingFuel = Initfuel - (FuelBurnRate * OneHourInSeconds) //calculates remaining fuel
-const NewVelocity = CalcNewVelocity(Acceleration , InitVelocity, OneHourInSeconds) //calculates new velocity based on acceleration
+const NewDistance = InitDistance + (InitVelocityMps * OneHourInSeconds)/1000 //calcultes new distance in Km
+const RemainingFuel = Initfuel - (FuelBurnRate * OneHourInSeconds) //calculates remaining fuel in kg
+const NewVelocity = CalcNewVelocity(Acceleration , InitVelocityMps, OneHourInSeconds) //calculates new velocity based on acceleration in km/h
 
 
 
